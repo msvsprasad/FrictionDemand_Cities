b82d2e90e38a3a3b2004c5c5520f534eebe08515
wushuangbai <wxb41@psu.edu>
2022-11-01 17:20:32 -0400

    Fix: Added ENU reference query

--- script_estimateAccelForSmoothTrafficTrajSCE_7DoFPPC.m
+++ script_estimateAccelForSmoothTrafficTrajSCE_7DoFPPC.m
@@ -26,7 +26,34 @@ addpath('./Utilities/UTM Lib');
 % Global variables for the vehicle model
 global flag_update global_acceleration
 
+%% Query and store ENU reference data
+enu_attributes = ['id, name, date_added, '...
+                  'latitude, longitude, altitude, geography, '...
+                  'epsg_code, latitude_std, longitude_std, altitude_std, '...
+                  'timestamp']; % attributes in the enu reference table
+
+% connect to the database
+db_name = 'nsf_roadtraffic_friction_v2';
+db_ip_address = '130.203.223.234'; % IP address of server host
+db_port = '5432';
+db_username = 'brennan';
+db_password = 'ivsg@Reber320';
+traffic_table = 'enu_reference';
+DB = Database(db_name,db_ip_address,db_port,...
+              db_username,db_password);
+
+% SQL statement to query vehicle trajectory
+enu_query = ['SELECT ' enu_attributes...
+             ' FROM ' traffic_table...
+                   ' ORDER BY date_added'];
+
+% query trajectory data from the DB
+enu_reference_table = fetch(DB.db_connection, enu_query);
+
+% Disconnect from the database
+DB.disconnect();
 %% Define inputs and parameters
+% define database for vehicle trajectories
 dbInput.ip_address = '130.203.223.234'; % Ip address of server host
 dbInput.port       = '5432'; % port number
 dbInput.username   = 'brennan'; % user name for the server
