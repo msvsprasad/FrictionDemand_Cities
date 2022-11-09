046536128b01710b81260168cb624d36a9234006
Juliette Mitrovich <82235686+JMitro@users.noreply.github.com>
2022-11-09 17:58:40 -0500

    Feat: function to test datapush

---
+++ Utilities/DB Lib/fcn_pushElevationDataToROIdb.m
@@ -0,0 +1,29 @@
+function fcn_pushElevationDataToROIdb(input_table_push_data)
+%% ------------------------ CONNECT TO DATABASE ------------------------ %%
+% Database parameters
+databasename = 'roi_db'; % database name
+username = 'brennan'; % user name for the server
+password = 'ivsg@Reber320'; % password
+driver = 'org.postgresql.Driver';   % JDBC Driver
+url    = ['jdbc:postgresql://130.203.223.234:5432/',databasename]; % This defines the IP address and port of the computer hosting the data (MOST important)
+
+% connect to databse
+conn = database(databasename,username,password,driver,url);
+
+% check the connection status and 
+% try to reconnect if the connection is not successful
+while size(conn.Message) ~= 0
+    fprintf('Trying to connect to the DB \n');
+    conn = database(databasename,username,password,driver,url);
+end
+fprintf('Connected to the DB \n');
+
+%% ----------------------- PUSH DATA TO DATABASE ----------------------- %%
+db_tablename = 'road_traffic_extend_2_matlab';  % relation name
+% insert data into the table
+sqlwrite(conn, db_tablename, input_table_push_data);
+
+%% ----------------------- DISCONNECT TO DATABASE ---------------------- %%
+close(conn);
+
+end
