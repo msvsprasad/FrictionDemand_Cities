90a271f0ab263061dd8f05ab097b0b9e2f39cd37
Juliette Mitrovich <82235686+JMitro@users.noreply.github.com>
2023-01-20 00:22:29 -0500

    Fix: minor fixes

--- Utilities/UTM Lib/fcn_addElevationAndConvertUTM2ENU_RCL.m
+++ Utilities/UTM Lib/fcn_addElevationAndConvertUTM2ENU_RCL.m
@@ -29,13 +29,18 @@
 % Created: 2023-01-17
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function road_centerline_section_ENU = fcn_addElevationAndConvertUTM2ENU_RCL(road_centerline_section,X,Y,lat0,lon0,h0,wgs84,elevation_map)
-%% Add elevation to the road centerline
+%% Allocate each column of road_centerline_section to individual varaibles
+sectionID = road_centerline_section(:,1);
+number_of_lanes = road_centerline_section(:,2);
+x_RCL = road_centerline_section(:,3);
+y_RCL = road_centerline_section(:,4);
+
 % set the minimun and maximum X and Y values to limit the nearest
 % neighbors search to just the specific trajectory
-position_front_x_min_RCL = min(road_centerline_section(:,2));
-position_front_x_max_RCL = max(road_centerline_section(:,2));
-position_front_y_max_RCL = max(road_centerline_section(:,3));
-position_front_y_min_RCL = min(road_centerline_section(:,3));
+position_front_x_min_RCL = min(x_RCL);
+position_front_x_max_RCL = max(x_RCL);
+position_front_y_max_RCL = max(y_RCL);
+position_front_y_min_RCL = min(y_RCL);
 % 
 % elevation_map_range_RCL = (X>=position_front_x_min_RCL & ...
 %     X<=position_front_x_max_RCL & ...
@@ -50,15 +55,15 @@ Ynew = Y(X>=position_front_x_min_RCL & X<=position_front_x_max_RCL & ...
 % elevation_map_new_RCL = elevation_map(elevation_map_range_RCL,:);
 
 % Find the nearest neighbors
-Idx = knnsearch([Xnew,Ynew],[road_centerline_section(:,2) road_centerline_section(:,3)],"K",2);
+Idx = knnsearch([Xnew,Ynew],[x_RCL y_RCL],"K",2);
 %%
 % interpolate the altitude (average it)
 if length(Idx)>=1 && length(Xnew)>1 && length(Ynew)>1
     path_vector = [Xnew(Idx(:,2))-Xnew(Idx(:,1)),...
         Ynew(Idx(:,2))-Ynew(Idx(:,1))];
     path_segment_length  = sum(path_vector.^2,2).^0.5;
-    point_vector = [road_centerline_section(:,2)-Xnew(Idx(:,1)),...
-        road_centerline_section(:,3)-Ynew(Idx(:,1))];
+    point_vector = [x_RCL-Xnew(Idx(:,1)),...
+        y_RCL-Ynew(Idx(:,1))];
     projection_distance  = (path_vector(:,1).*point_vector(:,1)+...
         path_vector(:,2).*point_vector(:,2))...
         ./path_segment_length; % Do dot product
@@ -68,15 +73,15 @@ if length(Idx)>=1 && length(Xnew)>1 && length(Ynew)>1
     alt = elevation_map(Idx(:,1),3) + (elevation_map(Idx(:,2),3) - elevation_map(Idx(:,1),3)).*percent_along_length;
     
     %% Convert UTM to LL
-    [lat_RCL, lon_RCL] = utm2ll(road_centerline_section(:,2),road_centerline_section(:,3),18);
+    [lat_RCL, lon_RCL] = utm2ll(x_RCL,y_RCL,18);
     
     %% Convert lla to enu
     % lat and lon is as querried from the database
     % height is alt
     [cg_east_RCL, cg_north_RCL, cg_up_RCL] = geodetic2enu(lat_RCL, lon_RCL,...
         alt, lat0, lon0, h0, wgs84);
-    road_centerline_section(:,2) = cg_east_RCL;
-    road_centerline_section(:,3) = cg_north_RCL;
+%     road_centerline_section(:,3) = cg_east_RCL;
+%     road_centerline_section(:,4) = cg_north_RCL;
 end
-road_centerline_section_ENU = [road_centerline_section(:,1) cg_east_RCL cg_north_RCL cg_up_RCL];
+road_centerline_section_ENU = [sectionID number_of_lanes cg_east_RCL cg_north_RCL cg_up_RCL];
 end
