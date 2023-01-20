7851f842222602a94cf7906cb2fa014b07f6a514
Juliette Mitrovich <82235686+JMitro@users.noreply.github.com>
2023-01-20 00:18:42 -0500

    Fix: added extraction of nb_lanes

--- Utilities/fcn_calculateRoadCenterlineSection.m
+++ Utilities/fcn_calculateRoadCenterlineSection.m
@@ -25,13 +25,18 @@
 %%
 function road_centerline_section = fcn_calculateRoadCenterlineSection(sections_shape)
 %% Extract ID values from shape file struct
-id_sections_shp = (extractfield(sections_shape,'id'))';
+SectionID_numberOfLanes = [(extractfield(sections_shape,'id'))',...
+    (extractfield(sections_shape,'nb_lanes'))'];
+
+% id_sections_shp = (extractfield(sections_shape,'id'))';
+% nb_lanes_shp = (extractfield(sections_shape,'nb_lanes'))';
 
 %% get the centerline data for network
 % initialize variable to store processed position orientation (pose) data
 road_centerline_section = [];
-list_of_sectionIds_yaw = id_sections_shp;
-for i = 1:length(list_of_sectionIds_yaw)
+list_of_sectionIds = SectionID_numberOfLanes(:,1);
+list_number_of_lanes = SectionID_numberOfLanes(:,2);
+for i = 1:length(list_of_sectionIds)
     % create the centerline path for each section ID
     centerline_path = [sections_shape(i).X', sections_shape(i).Y'];
 
@@ -40,7 +45,8 @@ for i = 1:length(list_of_sectionIds_yaw)
 
     % processed centerline data: section ID, path, yaw
     road_centerline_section = [road_centerline_section; ...
-        list_of_sectionIds_yaw(i)*ones(numel(centerline_path(:,1)),1),...
+        list_of_sectionIds(i)*ones(numel(centerline_path(:,1)),1),...
+        list_number_of_lanes(i)*ones(numel(centerline_path(:,1)),1),...
         centerline_path];
     
     % calculate yaw based off of the centerline path
